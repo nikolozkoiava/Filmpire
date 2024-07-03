7e621032de0cdf601bf8b0b5406d8781140028a2
Nikoloz Koiava <koiavanika0@gmail.com>
2024-07-03 18:43:13 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+
+import {
+  Actors,
+  MoviesInformation,
+  Moviess,
+  NavBar,
+  Profile,
+} from "./components";
+
+function App() {
+  return (
+    <div className="flex h-full">
+      <CssBaseline />
+      <NavBar />
+      <main className="flex-grow p-[2rem]">
+        <div className="h-16" />
+        <Routes>
+          <Route path="/movie/:id" element={<MoviesInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/" element={<Moviess />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Actors } from "./Actors/Actors";
+export { default as Moviess } from "./Moviess/Movies";
+export { default as MoviesInformation } from "./MovieInformation/MovieInformation";
+export { default as Profile } from "./Profile/Profile";
+export { default as NavBar } from "./NavBar/NavBar";
