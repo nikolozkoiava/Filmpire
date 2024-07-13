5b9297184b0fb307db8c0082c4affb8d8d2a5a04
Nikoloz Koiava <koiavanika0@gmail.com>
2024-07-13 23:58:08 +0400

    Start development of Movie Information page

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,5 +1,64 @@
+import {
+  Modal,
+  Typography,
+  Button,
+  ButtonGroup,
+  Grid,
+  Box,
+  CircularProgress,
+  useMediaQuery,
+  Rating,
+} from "@mui/material";
+import {
+  Movie as MovieIcon,
+  Theaters,
+  Language,
+  PlusOne,
+  Favorite,
+  FavoriteBorderOutlined,
+  Remove,
+  ArrowBack,
+} from "@mui/icons-material";
+
+import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+
+import { useGetMovieQuery } from "../../services/TMDB";
+
 const MovieInformation = () => {
-  return <div>MovieInformation</div>;
+  const { id } = useParams();
+  const { data, isFetching, error } = useGetMovieQuery(id);
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <CircularProgress size="8rem" />
+      </Box>
+    );
+  }
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Link to="/">Something has gone wrong - Go back</Link>
+      </Box>
+    );
+  }
+  return (
+    <Grid
+      container
+      className="flex-col justify-around my-[0.625rem] mx-0 sm:flex flex-wrap"
+    >
+      <Grid item sm={12} lg={4}>
+        <img
+          className=""
+          src={`https://image.tmdb.org/t/p/w500${data?.poster_path}`}
+          alt=""
+        />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default MovieInformation;
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -38,7 +38,13 @@ export const tmdbApi = createApi({
         return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
       },
     }),
+    //* Get Movie
+    getMovie: builder.query({
+      query: (id) =>
+        `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`,
+    }),
   }),
 });
 
-export const { useGetMoviesQuery, useGetGenresQuery } = tmdbApi;
+export const { useGetMoviesQuery, useGetGenresQuery, useGetMovieQuery } =
+  tmdbApi;
